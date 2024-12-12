57dacfae3e25f810512c3cc2a3b479514f384238
OswaldoBifarini <jgbifarini@gmail.com>
2024-12-12 15:58:38 -0500

    Actualizacion final

--- src/main/java/ec/edu/espoc/libreria/cliente/Cliente.java
+++ src/main/java/ec/edu/espoc/libreria/cliente/Cliente.java
@@ -4,7 +4,7 @@ public class Cliente {
 
     private int idCliente;
     private String nombre;
-
+    
     public Cliente(int idCliente, String nombre) {
         this.idCliente = idCliente;
         this.nombre = nombre;
