d75ec562c096e75d1f12d1ea4577bfdfef9617b6
OswaldoBifarini <jgbifarini@gmail.com>
2024-12-12 14:36:27 -0500

    Creación de Repositorio

---
+++ src/main/java/ec/edu/espoc/libreria/Libreria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoc.libreria;
+
+/**
+ *
+ * @author DELL
+ */
+public class Libreria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
